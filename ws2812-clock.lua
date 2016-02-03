48465f252c474c3e19b14e18a4b44b725300fed8
cyberjon526 <jonathan@jlcowan.com>
2016-02-03 01:16:38 -0500

    Update ws2812-clock.lua

--- ws2812-clock.lua
+++ ws2812-clock.lua
@@ -20,29 +20,6 @@ ohr = hour-thr*10
 tmin = math.floor(minute/10)
 omin = minute - (tmin*10)
 
--- arrarys for display
-number = {} -- define number array
-colon = {}
-
--- if you want different color beisdes white, change following _on variables for example red = \010\000\000 make integer larger than 10 to increase brightness
-l_on = '\010\010\010\010\010\010\010\010\010\010\010\010\010\010\010'
-l_off = '\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000'
-s_on = '\010\010\010\010\010\010\010\010\010\010\010\010'
-s_off = '\000\000\000\000\000\000\000\000\000\000\000\000'
-
-number[1] = s_off .. l_on .. l_on .. s_off .. l_off .. l_off .. s_off
-number[2] = s_on .. l_on .. l_off .. s_on .. l_on .. l_off .. s_on
-number[3] = s_on .. l_on .. l_on .. s_on .. l_off .. l_off .. s_on
-number[4] = s_off .. l_on .. l_on .. s_off .. l_off .. l_on .. s_on
-number[5] = s_on .. l_off .. l_on .. s_on .. l_off .. l_on .. s_on
-number[6] = s_on .. l_off .. l_on .. s_on .. l_on .. l_on .. s_on
-number[7] = s_on .. l_on .. l_on .. s_off .. l_off .. l_off .. s_off
-number[8] = s_on .. l_on .. l_on .. s_on .. l_on .. l_on .. s_on
-number[9] = s_on .. l_on .. l_on .. s_on .. l_off .. l_on .. s_on
-number[0] = s_on .. l_on .. l_on .. s_on .. l_on .. l_on .. s_off
-colon[0] = s_off
-colon[1] = s_on
-
 -- write data to led strips
 ws2812.writergb(digits, number[thr] .. number[ohr] .. number[tmin] .. number[omin])
 ws2812.writergb(coln, colon[tick])
