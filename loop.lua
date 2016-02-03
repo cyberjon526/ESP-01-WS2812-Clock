4d2825f3b6bd86defb9fd864a45af35c59494e7b
cyberjon526 <jonathan@jlcowan.com>
2016-02-02 22:03:06 -0500

    Update loop.lua

--- loop.lua
+++ loop.lua
@@ -3,5 +3,5 @@ dofile("loadNTP.lua")
 tmr.delay(2000000)
 dofile("telnet.lua")
 
---loop for infinity
+--loop for infinity -- execute ws2812-clock.lua every 250ms
 tmr.alarm(0, 250, 1, function() dofile("ws2812-clock.lua") end )
