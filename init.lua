4ffe1571273ad7295825df4cd6b6dc6dd94a52e4
cyberjon526 <jonathan@jlcowan.com>
2016-02-02 21:53:46 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 print("Setting up WIFI...")
 wifi.setmode(wifi.STATION)
 --modify according your wireless router settings
-wifi.sta.config("SSIDe","Password") -- replace SSID and Password with your network settings
+wifi.sta.config("SSID","Password") -- replace SSID and Password with your network settings
 wifi.sta.connect()
 tmr.alarm(1, 1000, 1, function() 
 if wifi.sta.getip()== nil then 
